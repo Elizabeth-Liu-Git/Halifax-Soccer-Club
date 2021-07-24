f50814db7c0dba46c33e9350633444b8d2d3f6da
Aoxiang Zhang <ax706429@dal.ca>
2021-07-23 22:30:34 -0300

    retrieve pics from S3

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -2,7 +2,57 @@
 
   <div>
 
-  <h2>Photos and Videos from AWS S3</h2>
-   <img> 
+  <h2 class ="h2">Photos and Videos from AWS S3</h2>
+
+  
+  <div id="img-slider" class ='slider'>
+    <vue-image-slider :images="imageArray" :intervalVal=3000 :height=700 :width=1200 />
+  </div>
+
+
   </div>
 </template>
+
+
+
+<script>
+ import VueImageSlider from 'vue-image-slider'
+ 
+export default {
+ name: 'app',
+ components: {
+  VueImageSlider
+ },
+ data() {
+  return {
+   imageArray: [
+     // to add more pics here
+    'https://s3.amazonaws.com/picuploadsnew/1626998210906.jpg',
+     'https://picuploadsnew.s3.amazonaws.com/1626998881427.jpg',
+     'https://picuploadsnew.s3.amazonaws.com/1627071474177.jpg'
+   ],
+  }
+ },
+}
+</script>
+
+<style scoped>
+
+.h2{
+  font-size: 5rem;
+}
+
+.slider{
+
+  display: flex;
+  margin-left: auto;
+  margin-right: auto;
+  width: 60%;
+  height:auto;
+
+}
+
+</style>
+
+
+
