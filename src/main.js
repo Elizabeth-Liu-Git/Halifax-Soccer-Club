934c827f87545bf254262feeaddb1f7d0dfcad3f
Aoxiang Zhang <ax706429@dal.ca>
2021-07-23 22:31:16 -0300

    deleted unused library

--- src/main.js
+++ src/main.js
@@ -3,18 +3,15 @@ import VueRouter from 'vue-router'
 import Amplify from 'aws-amplify'
 import '@aws-amplify/ui-vue'
 import config from './aws-exports';
-import VueAwesomeSwiper from 'vue-awesome-swiper';
 
 
 import App from './App'
 import router from './router'
-import 'swiper/css/swiper.css'
 
 
 
 Amplify.configure(config)
 Vue.use(VueRouter)
-Vue.use(VueAwesomeSwiper,)
 Vue.config.productionTip = false
 
 new Vue({
